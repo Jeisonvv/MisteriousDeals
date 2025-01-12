9cc723d6c5dd0e188e60f8a82512640ed4005654
jeisonvv <jeisonhvargaav@gmail.com>
2025-01-12 16:04:53 -0500

    p

--- whatsapp-bot/src/clinet.js
+++ whatsapp-bot/src/clinet.js
@@ -1,9 +1,7 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
-    authStrategy: new LocalAuth({
-        dataPath: './sessions', // Ruta donde se guardará la sesión
-    }),
+    authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true, // Cambiar a false si deseas ver el navegador en ejecución
     },
